b1284b63cefc9f06cfe99a11440edf78bd785a1a
SamiS0830 <samanthashapiro27@marlborough.org>
2025-10-16 09:20:02 -0700

    HW

---
+++ main.js
@@ -0,0 +1,19 @@
+//
+// main.js
+//
+
+
+function myFunction() 
+{
+    window.alert("Hello, world!");
+}
+
+
+function calculate()
+{
+    let input = document.getElementById("my_input");
+    let result = input.value * input.value;
+    window.alert("result: " + result);
+}
+
+
